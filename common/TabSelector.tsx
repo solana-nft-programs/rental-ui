a7f0f22e395ba1face1bb088ddfaaa5a99a74adf
jpbogle <jpbogle22@gmail.com>
2022-07-14 01:50:35 -0400

    Results text

---
+++ common/TabSelector.tsx
@@ -0,0 +1,48 @@
+import { css } from '@emotion/react'
+import { lighten } from 'polished'
+import { useProjectConfig } from 'providers/ProjectConfigProvider'
+import { useState } from 'react'
+
+type Option<T> = { label: string; value: T }
+type Props<T> = {
+  placeholder?: string
+  options: Option<T>[]
+  defaultOption?: Option<T>
+  onChange?: (arg: Option<T>) => void
+}
+
+export const TabSelector = <T,>({
+  defaultOption,
+  onChange,
+  options = [],
+}: Props<T>) => {
+  const { config } = useProjectConfig()
+  const [value, setValue] = useState<Option<T> | undefined>(defaultOption)
+  console.log(value)
+  return (
+    <div
+      className="flex rounded-lg border-[1px] border-border"
+      css={css`
+        background: ${lighten(0.08, config.colors.main)};
+      `}
+    >
+      {options.map((o) => (
+        <div
+          key={o.label}
+          className="flex cursor-pointer items-center justify-between rounded-lg px-5 py-2 text-sm text-light-0 transition-colors hover:text-primary"
+          css={css`
+            background: ${value?.value === o.value
+              ? lighten(0.03, config.colors.main)
+              : ''};
+          `}
+          onClick={() => {
+            setValue(o)
+            onChange && onChange(o)
+          }}
+        >
+          <div>{o.label}</div>
+        </div>
+      ))}
+    </div>
+  )
+}
