b11401f543c158816f4b7f1b22b61b08433b992d
jpbogle <jpbogle22@gmail.com>
2022-07-19 19:43:19 -0400

    Expiration badge and general cleanup

--- common/Tags.tsx
+++ common/Tags.tsx
@@ -1,6 +1,7 @@
 import type { TokenManagerState } from '@cardinal/token-manager/dist/cjs/programs/tokenManager'
 import styled from '@emotion/styled'
-import { stateColor } from 'common/NFTOverlay'
+
+import { stateColor } from './NFT'
 
 export const Tag = styled.div<{ state: TokenManagerState }>`
   color: ${({ state }) => stateColor(state, true)} !important;
