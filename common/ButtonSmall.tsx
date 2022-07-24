313e09f2a470c82e04c3dd1ae87ef9c10d6d2a4f
jpbogle <jpbogle22@gmail.com>
2022-07-24 15:10:34 -0400

    Button small updates

--- common/ButtonSmall.tsx
+++ common/ButtonSmall.tsx
@@ -1,57 +1,42 @@
 import { css } from '@emotion/react'
-import styled from '@emotion/styled'
 import { lighten } from 'polished'
 import { useProjectConfig } from 'providers/ProjectConfigProvider'
 import { useState } from 'react'
 import { LoadingSpinner } from 'rental-components/common/LoadingSpinner'
 
-export type ButtonSmallProps = {
-  disabled?: boolean
-  bgColor?: string
-}
-
-export type Props = {
+export interface Props extends React.HTMLAttributes<HTMLDivElement> {
   children: JSX.Element | string
   className?: string
   loading?: boolean
+  disabled?: boolean
   onClick?: () => void
-} & ButtonSmallProps
-
-export const StyledButtonSmall = styled.button<ButtonSmallProps>`
-  color: white;
-  cursor: ${({ disabled }) => !disabled && 'pointer'};
-  opacity: ${({ disabled }) => (disabled ? '0.5' : '1')};
-  border: none;
-  outline: none;
-  padding: 8px 12px;
-  white-space: break-spaces;
-  transition: background 0.2s, box-shadow 0.2s;
-  &:hover {
-    background: ${({ disabled }) => (!disabled ? '#1a1a1a' : '')};
-    box-shadow: ${({ disabled }) =>
-      !disabled
-        ? `0px 4px 8px -4px rgba(0, 0, 0, 0.05),
-      inset 0px -1px 1px rgba(0, 0, 0, 0.04),
-      inset 0px 2px 0px rgba(255, 255, 255, 0.05)`
-        : ''};
-  }
-`
+}
 
 export const ButtonSmall: React.FC<Props> = ({
   children,
   onClick,
   className,
   loading,
-  ...buttonProps
+  disabled,
+  ...props
 }: Props) => {
   const [loadingClick, setLoadingClick] = useState(false)
   const { config } = useProjectConfig()
   return (
-    <StyledButtonSmall
-      {...buttonProps}
-      className={`flex items-center justify-center gap-1 rounded-2xl text-xs ${className}`}
+    <div
+      {...props}
+      className={`flex items-center justify-center gap-1 rounded-xl border-[0px] border-border px-3 py-2 text-xs transition-all ${className} ${
+        disabled ? 'cursor-default' : 'cursor-pointer'
+      }`}
       css={css`
+        white-space: break-spaces;
         background: ${lighten(0.15, config.colors.main)};
+        opacity: ${disabled ? '.5' : '1'};
+        box-shadow: inset 2px 2px rgba(255, 255, 255, 0.1);
+        &:hover {
+          background: ${!disabled ? '#1a1a1a' : ''};
+          box-shadow: none;
+
       `}
       onClick={async () => {
         if (!onClick) return
@@ -64,6 +49,6 @@ export const ButtonSmall: React.FC<Props> = ({
       }}
     >
       {loadingClick || loading ? <LoadingSpinner height="25px" /> : children}
-    </StyledButtonSmall>
+    </div>
   )
 }
