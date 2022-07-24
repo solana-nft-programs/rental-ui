801a527128bf57c862ffe27f5545bb5e7ec4dfe8
Avinash Jain <avinashkalpitjain@gmail.com>
2022-07-24 15:56:39 -0400

    Copy updates

--- components/Collections.tsx
+++ components/Collections.tsx
@@ -1,8 +1,8 @@
 import { css } from '@emotion/react'
 import { GlyphPlus } from 'assets/GlyphPlus'
-import { MoneyGlow } from 'assets/MoneyGlow'
-import { RentGlow } from 'assets/RentGlow'
-import { WalletGlow } from 'assets/WalletGlow'
+import { MoneyGlow, MoneyGlowSecondary } from 'assets/MoneyGlow'
+import { RentGlow, RentGlowSecondary } from 'assets/RentGlow'
+import { WalletGlow, WalletGlowSecondary } from 'assets/WalletGlow'
 import { Card } from 'common/Card'
 import { FooterSlim } from 'common/FooterSlim'
 import { Glow } from 'common/Glow'
@@ -40,17 +40,20 @@ export const Collections = () => {
         <div className="flex flex-wrap justify-between gap-10 px-16 py-28">
           <Glow angle={130} scale={1.5}>
             <div className="flex flex-col gap-2">
-              <div className="text-5xl text-light-0">Rental Marketplace</div>
+              <div className="text-5xl text-light-0">
+                NFT Rental Marketplace
+              </div>
               <div className="text-lg text-medium-3">
-                Let your NFTs generate revenue and RENT
-                <br /> NFTs temporarily!
+                The first NFT Rental Marketplace on Solana. <br />
+                Hold the <b>actual NFT</b> and access its utility for the
+                duration rented.
               </div>
             </div>
           </Glow>
           <div className="flex flex-col items-end justify-end gap-5 ">
             <div className="flex items-center gap-6">
               <div className="text-lg text-medium-3">
-                Are you a product owner?
+                Are you a collection owner?
               </div>
               <div
                 className="flex cursor-pointer items-center gap-1 rounded-lg bg-primary p-3 text-light-0 transition-colors hover:bg-primary-hover"
@@ -169,54 +172,112 @@ export const Collections = () => {
           </div>
         ))}
       </div>
-      <div className="my-40 px-2 md:px-12">
-        <div className="my-12 flex items-center justify-center text-4xl text-light-0">
-          How it Works
+
+      <div className="my-24 px-2 md:px-12">
+        {/* <div className="my-12 flex items-center justify-center text-4xl text-light-0">
+          How It Works
+        </div> */}
+        <div className="mx-auto my-24 h-[2px] w-[90%] rounded-lg bg-border"></div>
+        <div className="px-10 mb-10">
+
+          <div className="mb-6 flex justify-center px-3 text-center text-4xl text-light-0">
+            Renting a Token
+          </div>
+          <div className="grid w-full grid-cols-1 flex-wrap gap-4  md:grid-cols-3">
+            {[
+              {
+                icon: <RentGlow />,
+                header: 'Rent an NFT',
+                description:
+                  'Browse our various collections and rent an NFT at its specified rate in just a few clicks',
+              },
+              {
+                icon: <MoneyGlow />,
+                header: 'Access Utility',
+                description:
+                  'Given that the actual NFT sits in your wallet while rented, you can access any utility it provides',
+              },
+              {
+                icon: <WalletGlow />,
+                header: 'Extend Duration',
+                description:
+                  "Some rentals are extendable! In many cases, you can optionally pay to extend the rental's duration",
+              },
+            ].map(({ icon, header, description }) => (
+              <div
+                key={header}
+                className="min-h-80 flex flex-col items-center rounded-3xl bg-white bg-opacity-5 py-12 px-4 xl:px-24"
+              >
+                <Glow blur={20} scale={3}>
+                  <div
+                    className="flex h-16 w-16 items-center justify-center rounded-2xl p-3"
+                    css={css`
+                      background: linear-gradient(
+                        180deg,
+                        rgba(184, 95, 255, 0.25) 0%,
+                        rgba(184, 95, 255, 0) 100%
+                      );
+                      box-shadow: 0px 0px 0px 3px rgba(255, 255, 255, 0.1) inset;
+                    `}
+                  >
+                    {icon}
+                  </div>
+                </Glow>
+                <div className="my-4 text-3xl text-light-0">{header}</div>
+                <div className="text-center text-medium-3">{description}</div>
+              </div>
+            ))}
+          </div>
         </div>
-        <div className="grid w-full grid-cols-1 flex-wrap gap-4 px-10 md:grid-cols-3">
-          {[
-            {
-              icon: <WalletGlow />,
-              header: 'Connect wallet',
-              description:
-                'Connect the Solana wallet of your choice by clicking the wallet icon in the top right corner',
-            },
-            {
-              icon: <RentGlow />,
-              header: 'Rent NFT',
-              description:
-                'Browse the collection and buy the NFT you want in a few clicks',
-            },
-            {
-              icon: <MoneyGlow />,
-              header: 'Generate revenue',
-              description:
-                'Set time based expiry or rate at which users can pay your to hold your NFT until it is securely returned back to you',
-            },
-          ].map(({ icon, header, description }) => (
-            <div
-              key={header}
-              className="min-h-80 flex flex-col items-center rounded-3xl bg-dark-6 py-12 px-4 xl:px-24"
-            >
-              <Glow blur={20} scale={3}>
-                <div
-                  className="flex h-16 w-16 items-center justify-center rounded-2xl p-3"
-                  css={css`
-                    background: linear-gradient(
-                      180deg,
-                      rgba(184, 95, 255, 0.25) 0%,
-                      rgba(184, 95, 255, 0) 100%
-                    );
-                    box-shadow: 0px 0px 0px 3px rgba(255, 255, 255, 0.1) inset;
-                  `}
-                >
-                  {icon}
-                </div>
-              </Glow>
-              <div className="my-4 text-3xl text-light-0">{header}</div>
-              <div className="text-center text-medium-3">{description}</div>
-            </div>
-          ))}
+        <div className="px-10">
+          <div className="mb-6 mt-20 flex justify-center px-3 text-center text-4xl text-light-0">
+            Listing a Token
+          </div>
+          <div className="grid w-full grid-cols-1 flex-wrap gap-4  md:grid-cols-3">
+            {[
+              {
+                icon: <RentGlowSecondary />,
+                header: 'List your NFT',
+                description:
+                  'Specify a price and rental parameters like expiration, duration or rate and list your NFT for rent',
+              },
+              {
+                icon: <MoneyGlowSecondary />,
+                header: 'Earn Passive Income',
+                description:
+                  'Users can pay to hold your NFT until it is securely returned back to you at the end of the rental period',
+              },
+              {
+                icon: <WalletGlowSecondary />,
+                header: 'Automatic Relisting',
+                description:
+                  'Rentals are automatically relisted by default until a lister-specified max expiration is reached.',
+              },
+            ].map(({ icon, header, description }) => (
+              <div
+                key={header}
+                className="min-h-80 flex flex-col items-center rounded-3xl bg-white bg-opacity-5 py-12 px-4 xl:px-24"
+              >
+                <Glow blur={20} scale={3}>
+                  <div
+                    className="flex h-16 w-16 items-center justify-center rounded-2xl p-3"
+                    css={css`
+                      background: linear-gradient(
+                        180deg,
+                        rgba(251, 192, 147, 0.25) 0%,
+                        rgba(184, 95, 255, 0) 100%
+                      );
+                      box-shadow: 0px 0px 0px 3px rgba(255, 255, 255, 0.1) inset;
+                    `}
+                  >
+                    {icon}
+                  </div>
+                </Glow>
+                <div className="my-4 text-3xl text-light-0">{header}</div>
+                <div className="text-center text-medium-3">{description}</div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
       <FooterSlim />
