be4367c5021423234ed0cbac08975fe1ef37defb
Avinash Jain <avinashkalpitjain@gmail.com>
2022-08-10 16:30:24 -0400

    Updated solset to issuer/guild

--- config/config.ts
+++ config/config.ts
@@ -92,56 +92,6 @@ export type ProjectConfig = {
 }
 
 export const projectConfigs: { [key: string]: ProjectConfig } = {
-  solset: {
-    name: 'solset',
-    displayName: 'Solset',
-    type: 'Collection',
-    websiteUrl: 'https://solset.io',
-    logoImage: '/logos/solset.png',
-    hero: '/logos/solset-hero.png',
-    colors: {
-      accent: '#FCB316',
-      glow: '#FCB316',
-    },
-    description:
-      'Solset.io is a plug and play experience that works out of the box and offers sniping speed and precision with a simple and effective user interface. Set your snipes and forget about them until you get notified about your purchases through telegram. Welcome to the new breed of on chain snipers on Solana, welcome to Solset.io.',
-    twitterHandle: '@solset_io',
-    socialLinks: [
-      {
-        icon: 'discord',
-        link: 'https://discord.gg/solset ',
-      },
-      {
-        icon: 'web',
-        link: 'https://solset.io/',
-      },
-      {
-        icon: 'twitter',
-        link: 'https://twitter.com/solset_io',
-      },
-    ],
-    badges: [{ badgeType: 'recent' }],
-    filter: {
-      type: 'creators',
-      value: ['GUzQipbCSFz77ZhskMzG9VhDJ7HGioQv4dPpJ6id2ZX2'],
-    },
-    rentalCard: {
-      invalidators: ['rate', 'duration'],
-      invalidationOptions: {
-        visibilities: ['public', 'private'],
-        durationOptions: ['hours', 'days', 'weeks'],
-        invalidationTypes: ['reissue'],
-        paymentMints: ['So11111111111111111111111111111111111111112'],
-        showClaimRentalReceipt: false,
-        setClaimRentalReceipt: false,
-        maxDurationAllowed: {
-          displayText: '6 weeks',
-          value: 3629000,
-        },
-      },
-    },
-    marketplaceRate: 'days',
-  },
   rooniverse: {
     name: 'rooniverse',
     displayName: 'Rooniverse',
@@ -1310,6 +1260,56 @@ export const projectConfigs: { [key: string]: ProjectConfig } = {
         maxDurationAllowed: {
           displayText: '12 weeks',
           value: 7258000,
+        },
+      },
+    },
+    marketplaceRate: 'days',
+  },
+  solset: {
+    name: 'solset',
+    displayName: 'Solset',
+    type: 'Guild',
+    websiteUrl: 'https://solset.io',
+    logoImage: '/logos/solset.png',
+    hero: '/logos/solset-hero.png',
+    colors: {
+      accent: '#FCB316',
+      glow: '#FCB316',
+    },
+    description:
+      'Solset.io is a plug and play experience that works out of the box and offers sniping speed and precision with a simple and effective user interface. Set your snipes and forget about them until you get notified about your purchases through telegram. Welcome to the new breed of on chain snipers on Solana, welcome to Solset.io.',
+    twitterHandle: '@solset_io',
+    socialLinks: [
+      {
+        icon: 'discord',
+        link: 'https://discord.gg/solset ',
+      },
+      {
+        icon: 'web',
+        link: 'https://solset.io/',
+      },
+      {
+        icon: 'twitter',
+        link: 'https://twitter.com/solset_io',
+      },
+    ],
+    badges: [{ badgeType: 'recent' }],
+    filter: {
+      type: 'issuer',
+      value: ['SLSETzAUois5Wfy1jAxEDPkWiwmLgRcsDSUV6GRGoR2'],
+    },
+    rentalCard: {
+      invalidators: ['rate', 'duration'],
+      invalidationOptions: {
+        visibilities: ['public', 'private'],
+        durationOptions: ['hours', 'days', 'weeks'],
+        invalidationTypes: ['reissue'],
+        paymentMints: ['So11111111111111111111111111111111111111112'],
+        showClaimRentalReceipt: false,
+        setClaimRentalReceipt: false,
+        maxDurationAllowed: {
+          displayText: '6 weeks',
+          value: 3629000,
         },
       },
     },
