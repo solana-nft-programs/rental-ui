d814de22432507ec54703788e97adf2ba243bad6
Jeremy Bogle <jpbogle22@gmail.com>
2022-08-18 22:49:45 -0400

    Fix twitter card (#154)

--- next.config.js
+++ next.config.js
@@ -1,17 +1,5 @@
 const { withSentryConfig } = require('@sentry/nextjs')
 
-// For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
-if (
-  !process.env.LD_LIBRARY_PATH ||
-  !process.env.LD_LIBRARY_PATH.includes(
-    `${process.env.PWD}/node_modules/canvas/build/Release:`
-  )
-) {
-  process.env.LD_LIBRARY_PATH = `${
-    process.env.PWD
-  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`
-}
-
 /** @type {import('next').NextConfig} */
 module.exports = withSentryConfig(
   {
