7a79781cd87d03b996c4f07e025fb3f3a8d0f65c
jpbogle <jpbogle22@gmail.com>
2022-09-12 00:56:56 -0400

    Attribute popover

--- common/NFTHeader.tsx
+++ common/NFTHeader.tsx
@@ -1,12 +1,16 @@
 import { DisplayAddress } from '@cardinal/namespaces-components'
 import { logConfigTokenDataEvent } from 'apis/amplitude'
 import type { TokenData } from 'apis/api'
+import { useMintMetadata } from 'hooks/useMintMetadata'
 import { useEnvironmentCtx } from 'providers/EnvironmentProvider'
 import { getLink, useProjectConfig } from 'providers/ProjectConfigProvider'
+import { AiOutlineEye, AiOutlineInfoCircle } from 'react-icons/ai'
+import { BsFillInfoCircleFill } from 'react-icons/bs'
 import { FaLink } from 'react-icons/fa'
 
 import { notify } from './Notification'
 import { getNameFromTokenData } from './tokenDataUtils'
+import { Tooltip } from './Tooltip'
 
 export const handleCopy = async (shareUrl: string) => {
   await navigator.clipboard.writeText(shareUrl)
@@ -25,31 +29,54 @@ export const NFTHeader: React.FC<NFTHeaderProps> = ({
 }: NFTHeaderProps) => {
   const { config } = useProjectConfig()
   const { secondaryConnection } = useEnvironmentCtx()
+  const metadata = useMintMetadata(tokenData).data
   return (
-    <div
-      className="flex w-full cursor-pointer flex-col justify-between"
-      onClick={(e) => {
-        if (!tokenData.tokenManager) return
-        e.stopPropagation()
-        logConfigTokenDataEvent('nft: click claim link', config, tokenData)
-        handleCopy(
-          getLink(
-            `/${config.name}/${tokenData.tokenManager?.pubkey.toBase58()}`
-          )
-        )
-      }}
-    >
-      <div className="flex items-center gap-2 font-bold">
-        <div className="w-fit overflow-hidden text-ellipsis whitespace-nowrap text-left text-lg">
-          {getNameFromTokenData(tokenData, 'Unknown')}
-        </div>
-        {tokenData.tokenManager && (
-          <div className="flex w-fit">
-            <FaLink />
+    <div className="flex w-full flex-col justify-between">
+      <div className="flex items-center justify-between">
+        <div
+          className="flex cursor-pointer items-center gap-2 font-bold"
+          onClick={(e) => {
+            if (!tokenData.tokenManager) return
+            e.stopPropagation()
+            logConfigTokenDataEvent('nft: click claim link', config, tokenData)
+            handleCopy(
+              getLink(
+                `/${config.name}/${tokenData.tokenManager?.pubkey.toBase58()}`
+              )
+            )
+          }}
+        >
+          <div className="w-fit overflow-hidden text-ellipsis whitespace-nowrap text-left text-lg">
+            {getNameFromTokenData(tokenData, 'Unknown')}
           </div>
-        )}
+          {tokenData.tokenManager && (
+            <div className="flex w-fit">
+              <FaLink />
+            </div>
+          )}
+        </div>
+        <Tooltip
+          title={
+            <div>
+              {metadata?.parsed.attributes?.map((attr, i) => (
+                <div
+                  key={i}
+                  className={`flex items-center justify-between gap-1 rounded-md py-1 text-sm text-light-0`}
+                >
+                  <div className="font-bold">{attr.trait_type}</div>
+                  <div className="text-light-2">{attr?.value}</div>
+                </div>
+              ))}
+            </div>
+          }
+        >
+          <div className="flex cursor-pointer items-center gap-1 text-sm text-light-2">
+            View
+            {/* <BsFillInfoCircleFill /> */}
+          </div>
+        </Tooltip>
       </div>
-      <div className="text-sm text-light-2">
+      <div className="flex items-center justify-between text-sm text-light-2">
         <DisplayAddress
           dark
           connection={secondaryConnection}
