f859baa3f8bfabdb2d9fb67d781fe3f1d9074fa3
jpbogle <jpbogle22@gmail.com>
2022-09-12 01:48:18 -0400

    Add hover over entire attribute info

--- common/NFTAttributeInfo.tsx
+++ common/NFTAttributeInfo.tsx
@@ -18,49 +18,49 @@ export const NFTAttributeInfo = ({ tokenData, className }: Props) => {
   )
   if (!attributesByTraitType) return <></>
   return (
-    <div
-      className={`${className} flex items-center gap-3 rounded-md text-light-0 ${
-        config.attributeDisplay && 'bg-dark-5 px-2 py-1'
-      }`}
-    >
-      {config.attributeDisplay && (
-        <div className="flex items-center gap-2">
-          {config.attributeDisplay.map(
-            ({ displayName, attributeName }, i) =>
-              attributesByTraitType[attributeName] && (
-                <>
-                  <div className="flex items-center gap-1">
-                    <div className="font-semibold">
-                      {displayName || attributeName}:{' '}
-                    </div>
-                    <div>{attributesByTraitType[attributeName]?.value}</div>
-                  </div>
-                  {config.attributeDisplay &&
-                    config.attributeDisplay?.length > 1 &&
-                    i !== config.attributeDisplay?.length - 1 && <div>|</div>}
-                </>
-              )
-          )}
+    <Tooltip
+      className={`cursor-pointer rounded-md text-light-0`}
+      title={
+        <div>
+          {metadata?.parsed.attributes?.map((attr, i) => (
+            <div
+              key={i}
+              className={`flex items-center justify-between gap-1 rounded-md py-[2px] text-sm text-light-0`}
+            >
+              <div className="font-bold">{attr.trait_type}</div>
+              <div className="text-light-2">{attr?.value}</div>
+            </div>
+          ))}
         </div>
-      )}
-      <Tooltip
-        className={`rounded-md text-light-0`}
-        title={
-          <div>
-            {metadata?.parsed.attributes?.map((attr, i) => (
-              <div
-                key={i}
-                className={`flex items-center justify-between gap-1 rounded-md py-[2px] text-sm text-light-0`}
-              >
-                <div className="font-bold">{attr.trait_type}</div>
-                <div className="text-light-2">{attr?.value}</div>
-              </div>
-            ))}
-          </div>
-        }
+      }
+    >
+      <div
+        className={`${className} flex items-center gap-3 rounded-md text-light-0 ${
+          config.attributeDisplay && 'bg-dark-5 px-2 py-1'
+        }`}
       >
+        {config.attributeDisplay && (
+          <div className="flex items-center gap-2">
+            {config.attributeDisplay.map(
+              ({ displayName, attributeName }, i) =>
+                attributesByTraitType[attributeName] && (
+                  <>
+                    <div className="flex items-center gap-1">
+                      <div className="font-semibold">
+                        {displayName || attributeName}:{' '}
+                      </div>
+                      <div>{attributesByTraitType[attributeName]?.value}</div>
+                    </div>
+                    {config.attributeDisplay &&
+                      config.attributeDisplay?.length > 1 &&
+                      i !== config.attributeDisplay?.length - 1 && <div>|</div>}
+                  </>
+                )
+            )}
+          </div>
+        )}
         <div
-          className={`flex cursor-pointer items-center gap-1 ${
+          className={`flex items-center gap-1 ${
             config.attributeDisplay
               ? 'scale-[1.25] text-light-2'
               : 'scale-[1.5] text-dark-6'
@@ -68,7 +68,7 @@ export const NFTAttributeInfo = ({ tokenData, className }: Props) => {
         >
           <BsFillInfoCircleFill />
         </div>
-      </Tooltip>
-    </div>
+      </div>
+    </Tooltip>
   )
 }
