e125182c61bd4a41d7f960d8313a1fd8d0e734f9
jpbogle <jpbogle22@gmail.com>
2022-10-03 05:54:12 -0400

    Add vesting tooltip

--- common/NFT.tsx
+++ common/NFT.tsx
@@ -9,7 +9,8 @@ import { useUTCNow } from 'providers/UTCNowProvider'
 
 import { NFTAttributeInfo } from './NFTAttributeInfo'
 import { NFTContexualMenu } from './NFTContexualMenu'
-import { getNameFromTokenData } from './tokenDataUtils'
+import { getNameFromTokenData, invalidationTypeInfo } from './tokenDataUtils'
+import { Tooltip } from './Tooltip'
 
 export const getExpiration = (
   tokenData: Pick<TokenData, 'timeInvalidator' | 'tokenManager'>,
@@ -46,6 +47,9 @@ interface NFTProps {
 export function NFT({ tokenData }: NFTProps) {
   const { UTCNow } = useUTCNow()
   const metadata = useMintMetadata(tokenData).data
+  const invalidationType = invalidationTypeInfo(
+    tokenData.tokenManager?.parsed.invalidationType
+  )
   return (
     <div className="relative min-w-full rounded-xl bg-dark-5">
       <NFTContexualMenu tokenData={tokenData} />
@@ -62,6 +66,18 @@ export function NFT({ tokenData }: NFTProps) {
               {/* {rentalTypePill(rentalType(tokenData))} */}
             </div>
           )
+        )}
+        {tokenData.tokenManager?.parsed.invalidationType ===
+          InvalidationType.Vest && (
+          <Tooltip title={invalidationType.tooltip}>
+            <div
+              className={`absolute bottom-3 left-3 z-20 flex cursor-pointer items-center justify-center gap-1 rounded-md bg-dark-5 px-2 py-1 text-sm text-sm text-light-0 text-medium-3`}
+            >
+              <div className={`${invalidationType.color}`}>
+                {invalidationType.disaplyName}
+              </div>
+            </div>
+          </Tooltip>
         )}
         <NFTAttributeInfo
           className={`absolute bottom-3 right-3 z-20`}
