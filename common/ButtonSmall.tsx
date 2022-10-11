96ce6ae2f21dab336607dc9a4d891b6481deee18
jpbogle <jpbogle22@gmail.com>
2022-10-11 13:39:09 -0400

    Move loading spinner

--- common/ButtonSmall.tsx
+++ common/ButtonSmall.tsx
@@ -1,13 +1,16 @@
 import { css } from '@emotion/react'
 import { useState } from 'react'
-import { LoadingSpinner } from 'rental-components/common/LoadingSpinner'
+
+import { LoadingSpinner } from './LoadingSpinner'
 
 export interface Props extends React.HTMLAttributes<HTMLDivElement> {
-  children: JSX.Element | string
+  children: React.ReactNode
   className?: string
   loading?: boolean
   disabled?: boolean
   accented?: boolean
+  inlineLoader?: boolean
+  loader?: React.ReactElement
 }
 
 export const ButtonSmall: React.FC<Props> = ({
@@ -16,13 +19,18 @@ export const ButtonSmall: React.FC<Props> = ({
   className,
   loading,
   disabled,
+  inlineLoader,
+  loader,
   ...props
 }: Props) => {
   const [loadingClick, setLoadingClick] = useState(false)
+  const loaderElement = loader || (
+    <LoadingSpinner height="24" className="flex items-center justify-center" />
+  )
   return (
     <div
       {...props}
-      className={`flex items-center justify-center gap-1 rounded-xl border-[0px] border-border bg-light-0 bg-opacity-10 px-3 py-2 transition-all ${className} ${
+      className={`flex items-center justify-center gap-1 rounded-xl border-[0px] border-border bg-white bg-opacity-10 px-3 py-2 transition-all ${className} ${
         disabled
           ? 'cursor-default opacity-50'
           : 'cursor-pointer hover:bg-opacity-5'
@@ -40,7 +48,18 @@ export const ButtonSmall: React.FC<Props> = ({
         }
       }}
     >
-      {loadingClick || loading ? <LoadingSpinner height="25px" /> : children}
+      {loading || loadingClick ? (
+        inlineLoader ? (
+          <div className="flex items-center justify-center gap-2">
+            {loaderElement}
+            {children}
+          </div>
+        ) : (
+          loaderElement
+        )
+      ) : (
+        children
+      )}
     </div>
   )
 }
