dbaebabac7b2510c4bc997a321d497a6835bd995
jpbogle <jpbogle22@gmail.com>
2022-10-28 01:09:29 -0400

    Skip sentry next config if no token is defined

--- next.config.js
+++ next.config.js
@@ -1,49 +1,48 @@
 const { withSentryConfig } = require('@sentry/nextjs')
 
-/** @type {import('next').NextConfig} */
-module.exports = withSentryConfig(
-  {
-    reactStrictMode: false,
-    env: {
-      MAINNET_PRIMARY: process.env.MAINNET_PRIMARY,
-      MAINNET_PRIMARY_BETA: process.env.MAINNET_PRIMARY_BETA,
-      MAINNET_SECONDARY: process.env.MAINNET_SECONDARY,
-      BASE_CLUSTER: process.env.BASE_CLUSTER || 'devnet',
-      NEXT_PUBLIC_BASE_URL:
-        process.env.NEXT_PUBLIC_BASE_URL || 'https://rent.cardinal.so',
-    },
-    async rewrites() {
-      return [
-        {
-          source: '/:path*{/}?',
-          has: [
-            {
-              type: 'host',
-              value: '(?<hostName>.*)',
-            },
-          ],
-          destination: '/:hostName/:path*',
-        },
-        {
-          source: '/:hostName/:collection/claim/:tokenManagerString*',
-          destination: '/:hostName/:collection/:tokenManagerString*',
-        },
-        {
-          source: '/:hostName/claim/:tokenManagerString*',
-          destination: '/:hostName/default/:tokenManagerString*',
-        },
-      ]
-    },
+const nextConfig = {
+  reactStrictMode: false,
+  env: {
+    MAINNET_PRIMARY: process.env.MAINNET_PRIMARY,
+    MAINNET_PRIMARY_BETA: process.env.MAINNET_PRIMARY_BETA,
+    MAINNET_SECONDARY: process.env.MAINNET_SECONDARY,
+    BASE_CLUSTER: process.env.BASE_CLUSTER || 'devnet',
+    NEXT_PUBLIC_BASE_URL:
+      process.env.NEXT_PUBLIC_BASE_URL || 'https://rent.cardinal.so',
   },
-  {
-    // Additional config options for the Sentry Webpack plugin. Keep in mind that
-    // the following options are set automatically, and overriding them is not
-    // recommended:
-    //   release, url, org, project, authToken, configFile, stripPrefix,
-    //   urlPrefix, include, ignore
+  async rewrites() {
+    return [
+      {
+        source: '/:path*{/}?',
+        has: [
+          {
+            type: 'host',
+            value: '(?<hostName>.*)',
+          },
+        ],
+        destination: '/:hostName/:path*',
+      },
+      {
+        source: '/:hostName/:collection/claim/:tokenManagerString*',
+        destination: '/:hostName/:collection/:tokenManagerString*',
+      },
+      {
+        source: '/:hostName/claim/:tokenManagerString*',
+        destination: '/:hostName/default/:tokenManagerString*',
+      },
+    ]
+  },
+}
 
-    silent: true, // Suppresses all logs
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options.
-  }
-)
+/** @type {import('next').NextConfig} */
+module.exports = withSentryConfig(nextConfig, {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+})
