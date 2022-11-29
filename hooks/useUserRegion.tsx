6e92cc6646747cc5debfb6d50ba7074f079bb453
jpbogle <jpbogle22@gmail.com>
2022-11-28 20:02:08 -0500

    Disable region

--- hooks/useUserRegion.tsx
+++ hooks/useUserRegion.tsx
@@ -9,21 +9,21 @@ export const useUserRegion = () => {
   }>(
     ['useUserRegion', defaultDisallowedRegions.map((r) => `${r.code}}`)],
     async () => {
-      const response = await fetch(
-        `https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.NEXT_PUBLIC_GEO_LOCATION_API_KEY}`
-      )
-      const json = (await response.json()) as {
-        country_code2: string
-        country_name: string
-      }
+      // const response = await fetch(
+      //   `https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.NEXT_PUBLIC_GEO_LOCATION_API_KEY}`
+      // )
+      // const json = (await response.json()) as {
+      //   country_code2: string
+      //   country_name: string
+      // }
       return {
-        countryName: json.country_name ?? 'Unknown',
-        countryCode: json.country_code2,
-        isAllowed:
-          !!json.country_code2 &&
-          !defaultDisallowedRegions.some(
-            (r: { code: string }) => r.code === json.country_code2
-          ),
+        countryName: 'Unknown',
+        countryCode: 'Unkown',
+        isAllowed: true,
+        // !!json.country_code2 &&
+        // !defaultDisallowedRegions.some(
+        //   (r: { code: string }) => r.code === json.country_code2
+        // ),
       }
     }
   )
